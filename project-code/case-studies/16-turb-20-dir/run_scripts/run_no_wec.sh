9345331398b9ee8b78fa05ca4b7fff8397bd8b90
jaredthomas68 <jaredthomas68@gmail.com>
2019-08-07 12:40:32 -0600

    slurm scripts for 16 and 38 turb runs

---
+++ project-code/case-studies/16-turb-20-dir/run_scripts/run_no_wec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=2:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores per sub-job(i.e. tasks)
+#SBATCH --mem-per-cpu=2G  # memory per CPU core
+#SBATCH -J "16 turbs snopt no wec"   # job name
+#SBATCH --mail-user=jaredthomas68@gmail.com   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --array=0-199     # job array of size 100
+#SBATCH -C rhel7
+
+echo ${SLURM_ARRAY_TASK_ID}
+wec_method_number=0
+model_number=1
+op_alg_number=0
+
+python3 opt_mstart.py ${SLURM_ARRAY_TASK_ID} $wec_method_number $model_number $op_alg_number
