35fa214fe425a78e560330a4f8204e5dd7817ac0
Jared J. Thomas <jaredthomas68@gmail.com>
2019-08-26 15:35:19 -0600

    changes to run scripts

--- project-code/case-studies/38-turb-36-dir/run_scripts/run_no_wec.sh
+++ project-code/case-studies/38-turb-36-dir/run_scripts/run_no_wec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --time=5:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores per sub-job(i.e. tasks)
+#SBATCH --time=12:00:00   # walltime
+#SBATCH --ntasks=4   # number of processor cores per sub-job(i.e. tasks)
 #SBATCH --mem-per-cpu=2G  # memory per CPU core
 #SBATCH -J "38 turbs snopt no wec"   # job name
 #SBATCH --mail-user=jaredthomas68@gmail.com   # email address
