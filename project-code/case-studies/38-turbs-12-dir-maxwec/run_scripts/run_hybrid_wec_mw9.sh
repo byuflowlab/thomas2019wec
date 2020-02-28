585e93c085c015457fc16c0732a8d2f04cc52280
jaredthomas68 <jaredthomas68@gmail.com>
2020-02-28 16:05:03 -0700

    more max wec study script edits

--- project-code/case-studies/38-turbs-12-dir-maxwec/run_scripts/run_hybrid_wec_mw9.sh
+++ project-code/case-studies/38-turbs-12-dir-maxwec/run_scripts/run_hybrid_wec_mw9.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=12:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores per sub-job(i.e. tasks)
 #SBATCH --mem-per-cpu=2G  # memory per CPU core
-#SBATCH -J "38 turbs snopt hybrid wec"   # job name
+#SBATCH -J "38 turbs snopt hybrid wec mw9"   # job name
 #SBATCH --mail-user=jaredthomas68@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
