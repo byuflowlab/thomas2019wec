86900f75d01fea49f09d619bb0e10b7b7a4a994b
jaredthomas68 <jaredthomas68@gmail.com>
2020-03-10 14:21:04 -0600

    corrections to bash scripts for wec step study

--- project-code/case-studies/38-turbs-12-dir-wecstep/run_scripts/run_angle_wec_mw10_ws10.sh
+++ project-code/case-studies/38-turbs-12-dir-wecstep/run_scripts/run_angle_wec_mw10_ws10.sh
@@ -17,4 +17,4 @@ op_alg_number=0
 maxwec=10
 wecstep=10
 
-python3 opt_mstart.py ${SLURM_ARRAY_TASK_ID} $wec_method_number $model_number $op_alg_number $maxwec
+python3 opt_mstart.py ${SLURM_ARRAY_TASK_ID} $wec_method_number $model_number $op_alg_number $maxwec $wecstep $wecstep
