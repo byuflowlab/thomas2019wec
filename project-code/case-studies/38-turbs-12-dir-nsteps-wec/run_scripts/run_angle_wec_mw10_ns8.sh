9313b50c36a2b62362fe59e0972eaef56c657680
jaredthomas68 <jaredthomas68@gmail.com>
2020-03-24 10:25:41 -0600

    run scripts for wec nsteps

---
+++ project-code/case-studies/38-turbs-12-dir-nsteps-wec/run_scripts/run_angle_wec_mw10_ns8.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores per sub-job(i.e. tasks)
+#SBATCH --mem-per-cpu=2G  # memory per CPU core
+#SBATCH -J "38 turbs snopt wec angle mw10, ns2"   # job name
+#SBATCH --mail-user=jaredthomas68@gmail.com   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --array=0-199     # job array of size 200
+
+echo ${SLURM_ARRAY_TASK_ID}
+wec_method_number=2
+model_number=1
+op_alg_number=0
+maxwec=10
+nsteps=2
+
+python3 opt_mstart.py ${SLURM_ARRAY_TASK_ID} $wec_method_number $model_number $op_alg_number $maxwec $nsteps
