27dc5d223516fc4a35815e8393ef0e8cec583fdf
jaredthomas68 <jaredthomas68@gmail.com>
2020-03-27 14:30:58 -0600

    correction to runs scripts

--- project-code/case-studies/38-turbs-12-dir-const-nsteps-maxwec/run_scripts/run_angle_wec_mw80_ns6.sh
+++ project-code/case-studies/38-turbs-12-dir-const-nsteps-maxwec/run_scripts/run_angle_wec_mw80_ns6.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores per sub-job(i.e. tasks)
 #SBATCH --mem-per-cpu=1G  # memory per CPU core
-#SBATCH -J 38 turbs max wec, constant nsteps. alg: snopt. wec method: angle. ns:6. mw:80. # job name
+#SBATCH -J '38 turbs max wec, constant nsteps. alg: snopt. wec method: angle. ns:6. mw:80. # job name'
 #SBATCH --mail-user=jaredthomas68@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
