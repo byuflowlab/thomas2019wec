2ffa5296ce686c2a8ddbcf15e70349e5dfa2cec7
Jared J. Thomas <jaredthomas68@gmail.com>
2020-03-27 15:00:31 -0600

    corrections to submit script

---
+++ project-code/case-studies/38-turbs-12-dir-const-nsteps-maxwec/run_scripts/submit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for i in {2..10} 
+do     
+    echo "run_hybrid_wec_mw${i}_ns6.sh"
+    sbatch run_hybrid_wec_mw${i}_ns6.sh
+    sleep 1
+done
+
+for i in {2..10} 
+do     
+    echo "run_diam_wec_mw${i}_ns6.sh"
+    sbatch run_diam_wec_mw${i}_ns6.sh
+    sleep 1
+done
+
+for i in {5..80..5} 
+do     
+    echo "run_angle_wec_mw${i}_ns6.sh"
+    sbatch run_angle_wec_mw${i}_ns6.sh
+    sleep 1
+done
