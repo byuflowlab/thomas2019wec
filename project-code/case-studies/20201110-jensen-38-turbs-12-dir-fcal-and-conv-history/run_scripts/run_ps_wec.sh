bba0e144cf011d61303b1091cbd09ae1e115b67c
jaredthomas68 <jaredthomas68@gmail.com>
2020-11-10 13:11:11 -0700

    adding jensen case studies

---
+++ project-code/case-studies/20201110-jensen-38-turbs-12-dir-fcal-and-conv-history/run_scripts/run_ps_wec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=72:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores per sub-job(i.e. tasks)
+#SBATCH --mem-per-cpu=3G  # memory per CPU core
+#SBATCH -J 'Jensen 38 t 12 d alg: ps. wec method: diam. 3/6 # job name'
+#SBATCH --mail-user=jaredthomas68@gmail.com   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --array=0-199     # job array of size 200
+
+echo ${SLURM_ARRAY_TASK_ID}
+
+model_number=2
+op_alg_number=2
+wec_method_number=1
+maxwec=3
+nsteps=6
+
+python3 opt_mstart.py ${SLURM_ARRAY_TASK_ID} $wec_method_number $model_number $op_alg_number $maxwec $nsteps
