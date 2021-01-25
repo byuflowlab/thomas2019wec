d3d403e507d0c68bd081926f17bfedb74e170107
jaredthomas68 <jaredthomas68@gmail.com>
2021-01-25 16:37:24 -0700

    edit compile convergence history for ps wec

--- project-code/case-studies/202101042132-alpso-runs-random-seed/run_scripts/compile_convergence_history.py
+++ project-code/case-studies/202101042132-alpso-runs-random-seed/run_scripts/compile_convergence_history.py
@@ -36,64 +36,93 @@ def get_alpso_history(filepath):
 output_directory_wec = "../output_files/snopt_wec_diam_max_wec_3_nsteps_6.000/"
 output_directory_snopt = "../output_files/snopt/"
 output_directory_ps = "../output_files/ps/"
+output_directory_pswec = "../output_files/pswec/"
 # Specify output file name
 output_file_wec = output_directory_wec + "convergence_histories.txt"
 output_file_snopt = output_directory_snopt + "convergence_histories.txt"
 output_file_ps = output_directory_ps + "convergence_histories.txt"
+output_file_pswec = output_directory_pswec + "convergence_histories.txt"
 
 # indicate how many optimization runs are to be compiled
 runs = 10
 
-# compile SNOPT+WEC convergence history
+# # compile SNOPT+WEC convergence history
+# for run_number in np.arange(0, runs):
+
+#     data_in = np.loadtxt(output_directory_wec+"convergence_history_run%i.txt" %(run_number))
+
+#     nits = data_in.shape[0]
+#     AEPopt = data_in[:, 0]
+#     AEPcalc = data_in[:, 1]
+
+#     f = open(output_file_wec, "a")
+#     if run_number == 0:
+#         header = "convergence history alternating row AEP (W) unadjusted, AEP (W) adjusted"
+#     else:
+#         header = ""
+#     np.savetxt(f, (AEPopt, AEPcalc), header=header)
+#     f.close()
+
+# # compile SNOPT convergence history
+# for run_number in np.arange(0, runs):
+
+#     data_in = np.loadtxt(output_directory_snopt+"convergence_history_run%i.txt" %(run_number))
+
+#     nits = data_in.shape[0]
+#     AEPopt = data_in[:, 0]
+#     AEPcalc = data_in[:, 1]
+
+#     f = open(output_file_snopt, "a")
+#     if run_number == 0:
+#         header = "convergence history alternating row AEP (W) unadjusted, AEP (W) adjusted"
+#     else:
+#         header = ""
+#     np.savetxt(f, (AEPopt, AEPcalc), header=header)
+#     f.close()
+
+# # compile ALPSO convergence history
+# for run_number in np.arange(0, runs):
+
+#     fcalls, obj = get_alpso_history(output_directory_ps + "ALPSO_summary_multistart_16turbs_directionalWindRose_20dirs_BPAModel_RunID%i_print.out" %(run_number))
+
+#     nits = len(obj)
+#     AEPcalc = obj*1E7
+
+#     f = open(output_file_ps, "a")
+#     if run_number == 0:
+#         header = "convergence history alternating row function calls, AEP (W)"
+#     else:
+#         header = ""
+#     np.savetxt(f, (fcalls, -AEPcalc), header=header)
+#     f.close()
+
+# compile ALPSO + WEC convergence history
+max_wec = 6.0
+nsteps = 6
+expansion_factors = np.linspace(1.0, max_wec, nsteps)
+expansion_factors = np.append(np.flip(expansion_factors), 1.0)
+ti_types = np.zeros(nsteps+1)
+ti_types[-1] = 5
 for run_number in np.arange(0, runs):
+    print("getting history for run %i" %(run_number))
+    fcalls = np.empty(0)
+    obj = np.empty(0)
 
-    data_in = np.loadtxt(output_directory_wec+"convergence_history_run%i.txt" %(run_number))
-
-    nits = data_in.shape[0]
-    AEPopt = data_in[:, 0]
-    AEPcalc = data_in[:, 1]
-
-    f = open(output_file_wec, "a")
-    if run_number == 0:
-        header = "convergence history alternating row AEP (W) unadjusted, AEP (W) adjusted"
-    else:
-        header = ""
-    np.savetxt(f, (AEPopt, AEPcalc), header=header)
-    f.close()
-
-# compile SNOPT convergence history
-for run_number in np.arange(0, runs):
-
-    data_in = np.loadtxt(output_directory_snopt+"convergence_history_run%i.txt" %(run_number))
-
-    nits = data_in.shape[0]
-    AEPopt = data_in[:, 0]
-    AEPcalc = data_in[:, 1]
-
-    f = open(output_file_snopt, "a")
-    if run_number == 0:
-        header = "convergence history alternating row AEP (W) unadjusted, AEP (W) adjusted"
-    else:
-        header = ""
-    np.savetxt(f, (AEPopt, AEPcalc), header=header)
-    f.close()
-
-# compile ALPSO convergence history
-for run_number in np.arange(0, runs):
-
-    fcalls, obj = get_alpso_history(output_directory_ps + "ALPSO_summary_multistart_16turbs_directionalWindRose_20dirs_BPAModel_RunID%i_print.out" %(run_number))
+    for i in np.arange(o, len(expansion_factors)):
+        fcallsi, obji = get_alpso_history(output_directory_pswec + "ALPSO_summary_multistart_38turbs_nantucketWindRose_12dirs_BPAModel_RunID%i_EF%.3f_TItype%i_print.out" %(run_number, expansion_factors[i], ti_types[i]))
+        fcalls = np.append(fcalls,fcallsi)
+        obj = np.append(obj,obji)
 
     nits = len(obj)
     AEPcalc = obj*1E7
 
-    f = open(output_file_ps, "a")
+    f = open(output_file_pswec, "a")
     if run_number == 0:
         header = "convergence history alternating row function calls, AEP (W)"
     else:
         header = ""
     np.savetxt(f, (fcalls, -AEPcalc), header=header)
     f.close()
-
 
 
 
