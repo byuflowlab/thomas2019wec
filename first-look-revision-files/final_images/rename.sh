a2787f36911cc5fcd818d17489b80d8cd816a91b
jaredthomas68 <jaredthomas68@gmail.com>
2021-10-13 17:49:55 -0600

    revisions post first look acceptance

---
+++ first-look-revision-files/final_images/rename.sh
@@ -0,0 +1,11 @@
+#!
+
+mv tests/maxwec_const_nsteps6_mean.pdf tests/Figure_17.pdf
+mv tests/nsteps_const_maxwec_mean.pdf tests/Figure_18.pdf
+
+mv results/dist_boxpercentwakeloss.pdf results/Figure_19.pdf
+mv results/convergence_history_BPAmodel_16turbs_20dirs.pdf results/Figure_20.pdf
+mv results/convergence_history_BPAmodel_38turbs_12dirs.pdf results/Figure_21.pdf 
+mv results/convergence_history_JENSENmodel_38turbs_12dirs.pdf results/Figure_22.pdf
+mv results/convergence_history_BPAmodel_38turbs_36dirs.pdf results/Figure_23.pdf
+mv results/convergence_history_BPAmodel_60turbs_72dirs.pdf results/Figure_24.pdf
